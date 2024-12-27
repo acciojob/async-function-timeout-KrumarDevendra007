487f8d02b105041ab2b06d393195c0f116a91df1
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-27 21:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+const text = document.getElementById("text");
+const delay = document.getElmentById("delay");
+const output = document.getElementById("output");
+
+funtion getData() {
+	return new Promise((resolve()) => {
+		setTimeOut(() => {
+			resolve(() => {
+				output.innerHTML = `${text}`			
+			});
+		}, delay);
+	});
+}
+
+async funtion fetchData(){
+	try{
+	  const data = await getData();
+	}
+	catch (error){
+		console.log(error);
+	}
+}
